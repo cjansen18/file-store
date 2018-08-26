966b6c5277dd4e1b4b00a1189e11bad85364f0ac
Chris Jasnen <cjansen18@gmail.com>
2018-08-26 19:46:25 -0400

    Initial commit

---
+++ server/src/main/java/com/chrisjansen/filestore/SpringBootFileUploadApplication.java
@@ -0,0 +1,30 @@
+package com.chrisjansen.filestore;
+
+import javax.annotation.Resource;
+
+import com.chrisjansen.filestore.storage.StorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadApplication implements CommandLineRunner {
+
+	@Resource
+    StorageService storageService;
+
+	@Value("${delete.on.startup}")
+	boolean deleteOnStartupFlag;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		if(deleteOnStartupFlag)
+			storageService.deleteAll();
+		storageService.init();
+	}
+}
